aeb05b55a1ef0a6d5df87ae4bdc81cff1bb7f8b9
Ann7718 <111851476+Ann7718@users.noreply.github.com>
2023-12-16 18:23:28 +0200

    goit-js-hw-01

--- js/task-3.js
+++ js/task-3.js
@@ -5,9 +5,9 @@ function getElementWidth(content, padding, border){
     const padding1 = Number.parseFloat(padding);
     const border1 = Number.parseFloat(border);
     const boxSizing = content1 + (padding1 + border1) * 2;
-    console.log(boxSizing);
+    return boxSizing;
 }
 
-getElementWidth("50px", "8px", "4px")
-getElementWidth("60px", "12px", "8.5px")
-getElementWidth("200px", "0px", "0px")
+console.log(getElementWidth("50px", "8px", "4px")); // 74
+console.log(getElementWidth("60px", "12px", "8.5px")); // 101
+console.log(getElementWidth("200px", "0px", "0px")); // 200
